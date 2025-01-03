9278746e69d2597dd8bd82721fa46ef3c45689f6
HKS 27D <51793958+hks27d@users.noreply.github.com>
2025-01-03 21:33:00 +0000

    Initial release

---
+++ CSSharpHideAdminEvents.cs
@@ -0,0 +1,43 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Admin;
+
+namespace CSSharpHideAdminEvents
+{
+    public class CSSharpHideAdminEvents : BasePlugin
+    {
+        public override string ModuleName => "CS# Hide Admin Events";
+        public override string ModuleVersion => "1.0.0";
+        public override string ModuleAuthor => "HKS 27D";
+        public override string ModuleDescription => "";
+
+        public override void Load(bool hotReload)
+        {
+            RegisterEventHandler((EventPlayerTeam @event, GameEventInfo info) =>
+            {
+                CCSPlayerController JoinTeamPlayer = @event.Userid!;
+
+                if (JoinTeamPlayer != null && JoinTeamPlayer.IsValid && JoinTeamPlayer.PlayerPawn.IsValid && !JoinTeamPlayer.IsBot)
+                {
+                    if (AdminManager.PlayerHasPermissions(JoinTeamPlayer, "@css/generic"))
+                        info.DontBroadcast = true;
+                }
+
+                return HookResult.Continue;
+            }, HookMode.Pre);
+
+            RegisterEventHandler((EventPlayerDisconnect @event, GameEventInfo info) =>
+            {
+                CCSPlayerController DisconnectedPlayer = @event.Userid!;
+
+                if (DisconnectedPlayer != null && DisconnectedPlayer.IsValid && DisconnectedPlayer.PlayerPawn.IsValid && !DisconnectedPlayer.IsBot)
+                {
+                    if (AdminManager.PlayerHasPermissions(DisconnectedPlayer, "@css/generic"))
+                        info.DontBroadcast = true;
+                }
+
+                return HookResult.Continue;
+            }, HookMode.Pre);
+        }
+    }
+};
---
+++ README.md
@@ -0,0 +1,27 @@
+[![Discord](https://img.shields.io/discord/1323042994437357599?style=for-the-badge)](https://discord.com/invite/zsmUzthPXx)
+
+# 📌 CSSharp Hide Admin Events
+Hide admin info on PlayerTeam and PlayerDisconnect events.
+
+## 🌐 Description
+Most of the server owners encounter situations where cheaters or other types of players check the in-game chat for team join or disonnect. Why? For the presence of known admins. This plugin hide them on those events. Therefore, admins get a few more seconds or even minutes before their presence is observed by others when they join a team. The same concept applies on disconnection.
+
+## 📗 Dependecies
+- [CounterStrikeSharp](https://github.com/roflmuffin/CounterStrikeSharp)
+
+## 📄 Installation
+1. Get an IDE & Code Editor (Visual Studio, Rider or others).
+2. Edit and debug if you need to.
+3. Build the code.
+4. Add the files (`.dll`, `.pdb`, `.deps.json`) in their own directory.
+5. Rename directory. Use the same name.
+6. Upload directory in `/game/csgo/addons/counterstrikesharp/plugins`
+
+## 🤝 Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## ⭐ Show your support
+Give a ⭐ if this project helped you.
+
+## 📝 License
+MIT based on outlined exemption from CounterStrikeSharp.
